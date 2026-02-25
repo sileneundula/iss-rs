7c32bc9b0e828eb19e4586952f211b0d53e3844e
silene0259 <158784347+sileneundula@users.noreply.github.com>
2026-02-25 10:12:04 -0500

    Add ISS-CHAIN

---
+++ iss-chain-framework/src/lib.rs
@@ -0,0 +1,8 @@
+//! # Immutable Storage Solution: Chain Framework
+//! 
+//! The core components for the chain
+
+
+
+pub mod framework;
+pub mod essentials;
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,7 @@
+/// # Storage Size (Config)
+/// Unsigned4: 0-F
+/// Unsigned8: 00-FF
+/// Unsigned16: 0000-FFFF
 pub enum StorageSize {
     Unsigned4, // Nibble, 4 bits | 0-F
     Unsigned8, // Byte, 8 bits (1 byte)
